d7e94fdcd7fddd991b577799421784530e39815b
Nicholas Johnson <me@nicholasjohnson.com>
2019-07-16 11:32:18 +0100

    Create cache.ts

---
+++ cache.ts
@@ -0,0 +1,15 @@
+const _cache = {};
+
+const cache = {
+  add(url, data) { 
+    _cache[url] = data; 
+  },
+  get: url => _cache[url];
+  check: url => !!this.get(url),
+  remove(url) => delete _cache[url],
+  clear() { 
+    for (const url in _cache) {
+      this.remove(url);
+    };
+  }
+}
